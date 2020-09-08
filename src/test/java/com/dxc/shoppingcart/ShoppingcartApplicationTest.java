77d832c61d77c95f64111fe1a0b852cad89f17a3
P <ajithkumar.p@dxc.com>
2020-09-08 19:37:23 +0530

    first commit

---
+++ src/test/java/com/dxc/shoppingcart/ShoppingcartApplicationTest.java
@@ -0,0 +1,12 @@
+package com.dxc.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class ShoppingcartApplicationTest {
+
+		@Test
+		void contextLoads() {
+		}
+
+	}
